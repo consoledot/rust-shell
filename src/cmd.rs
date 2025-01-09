7a51b38dcfa2d5393728728314c70a43c9da4620
Abimbola Oladele <31113550+consoledot@users.noreply.github.com>
2025-01-09 10:24:09 +0100

    format code

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,6 +1,4 @@
-use std::{env, fs::{self, read_dir, File}, io, path::PathBuf};
-
-
+use std::{ env, fs::{ self, read_dir, File }, io, path::PathBuf };
 
 pub struct Cmd {
     command: String,
@@ -8,167 +6,134 @@ pub struct Cmd {
 }
 
 impl Cmd {
-    pub fn new(command: &str) -> Cmd{
-        Cmd{command:command.to_string(), args:None}
+    pub fn new(command: &str) -> Cmd {
+        Cmd { command: command.to_string(), args: None }
     }
-    pub fn args(&self, args: &[&str]) -> Cmd{
-        Cmd{
+    pub fn args(&self, args: &[&str]) -> Cmd {
+        Cmd {
             command: self.command.clone(),
-            args: Some(args.iter().map(|&s| s.to_string()).collect())
+            args: Some(
+                args
+                    .iter()
+                    .map(|&s| s.to_string())
+                    .collect()
+            ),
         }
-
     }
-    pub fn run(&self){
+    pub fn run(&self) {
+        match self.command.as_str() {
+            "ls" => self.peak_directory(),
+            "pwd" => {
+                let path: PathBuf = Self::get_current_dir();
+                match path.to_str() {
+                    Some(path) => println!("{}", path),
+                    None => println!("Unknown path"),
+                }
 
-    
-            match self.command.as_str() {
-                "ls"=> self.peak_directory(),
-                "pwd"=> {
-                   let path: PathBuf = Self::get_current_dir();
-                   match path.to_str() {
-                    Some(path)=> println!("{}", path),
-                    None => println!("Unknown path")
-                       
-                   }
-
-                
                 //    println!("{:#?}", path.to_str())
-                },
-                "torch"=> self.create_file(),
-                "rm"=> self.delete_file(),
-                "mkdir"=> self.create_directory(),
-                "rmdir"=> self.delete_directory(),
-                _ => println!("Unknown command")
-                   
-                
-
-                
             }
-        
+            "torch" => self.create_file(),
+            "rm" => self.delete_file(),
+            "mkdir" => self.create_directory(),
+            "rmdir" => self.delete_directory(),
+            _ => println!("Unknown command"),
+        }
     }
 
-    pub fn get_current_dir()-> PathBuf{
+    pub fn get_current_dir() -> PathBuf {
         match env::current_dir() {
-            Ok(path)=> {
-                
-                path
-            },
-            Err(err)=>{
+            Ok(path) => { path }
+            Err(err) => {
                 println!("Error getting current directory");
                 PathBuf::new()
             }
-            
         }
-           
     }
-    fn peak_directory(&self){
-        let mut files: Vec<PathBuf>= Vec::new();
+    fn peak_directory(&self) {
+        let mut files: Vec<PathBuf> = Vec::new();
         let path = Self::get_current_dir();
-        for entry in read_dir(path).unwrap(){
+        for entry in read_dir(path).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
-    
+
             if path.is_file() {
                 files.push(path);
             }
         }
-       for file in files{
-        println!("{}", file.components().last().map(|component| component.as_os_str().to_str())
-        .flatten().unwrap());
-       }
+        for file in files {
+            println!(
+                "{}",
+                file
+                    .components()
+                    .last()
+                    .map(|component| component.as_os_str().to_str())
+                    .flatten()
+                    .unwrap()
+            );
+        }
 
         // Ok(())
-        
     }
 
-    fn create_file(&self){
-      
+    fn create_file(&self) {
         match &self.args {
-           Some(arg)=>{
-            if (arg.is_empty()){
-                println!("Enter a valid filename");
-                return;
+            Some(arg) => {
+                if arg.is_empty() {
+                    println!("Enter a valid filename");
+                    return;
+                }
+                match File::create(arg[0].clone()) {
+                    Ok(file) => (),
+                    Err(err) => println!("enter a valid file name"),
+                }
             }
-            match File::create(arg[0].clone())  {
-                Ok(file) => (),
-                Err(err)=> println!("enter a valid file name", ),
-                
-            }
-           },
-           None => {
-            println!("File not found")
-           }
-            
+            None => { println!("File not found") }
         }
-   
-
     }
 
-
-    fn delete_file(&self){
-      
+    fn delete_file(&self) {
         match &self.args {
-           Some(arg)=>{
-            if (arg.is_empty()){
-                println!("Enter a valid filename");
-                return;
+            Some(arg) => {
+                if arg.is_empty() {
+                    println!("Enter a valid filename");
+                    return;
+                }
+                match fs::remove_file(arg[0].clone()) {
+                    Ok(file) => (),
+                    Err(err) => println!("enter a valid file name"),
+                }
             }
-            match fs::remove_file(arg[0].clone())  {
-                Ok(file) => (),
-                Err(err)=> println!("enter a valid file name", ),
-                
-            }
-           },
-           None => {
-            println!("File not found")
-           }
-            
+            None => { println!("File not found") }
         }
-   
-
     }
-    fn create_directory(&self){
-      
+    fn create_directory(&self) {
         match &self.args {
-           Some(arg)=>{
-            if (arg.is_empty()){
-                println!("Enter a valid directory name");
-                return;
+            Some(arg) => {
+                if arg.is_empty() {
+                    println!("Enter a valid directory name");
+                    return;
+                }
+                match fs::create_dir(arg[0].clone()) {
+                    Ok(file) => (),
+                    Err(err) => println!("enter a valid directory name"),
+                }
             }
-            match fs::create_dir(arg[0].clone())  {
-                Ok(file) => (),
-                Err(err)=> println!("enter a valid directory name", ),
-                
-            }
-           },
-           None => {
-            println!("File not found")
-           }
-        
+            None => { println!("File not found") }
         }
-   
-
     }
-    fn delete_directory(&self){
-      
+    fn delete_directory(&self) {
         match &self.args {
-           Some(arg)=>{
-            if (arg.is_empty()){
-                println!("Enter a valid directory name");
-                return;
+            Some(arg) => {
+                if arg.is_empty() {
+                    println!("Enter a valid directory name");
+                    return;
+                }
+                match fs::remove_dir(arg[0].clone()) {
+                    Ok(file) => (),
+                    Err(err) => println!("enter a valid directory name"),
+                }
             }
-            match fs::remove_dir(arg[0].clone())  {
-                Ok(file) => (),
-                Err(err)=> println!("enter a valid directory name", ),
-                
-            }
-           },
-           None => {
-            println!("File not found")
-           }
-        
+            None => { println!("File not found") }
         }
-   
-
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,28 @@
-use std::io::{self, Write};
+use std::io::{ self, Write };
 
 use rust_shell::Cmd;
 
 fn main() {
-        loop {
-            let  path = Cmd::get_current_dir();
-          let path =  path.components().last().map(|component| component.as_os_str().to_str())
-          .flatten();
-          if path.is_none(){
+    loop {
+        let path = Cmd::get_current_dir();
+        let path = path
+            .components()
+            .last()
+            .map(|component| component.as_os_str().to_str())
+            .flatten();
+        if path.is_none() {
             println!("Path is empty");
             break;
-          }
-            print!("{}: ",path.unwrap());
-            io::stdout().flush().unwrap();
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect("Enter a command");
-            let inputs:Vec<&str> = input.split_whitespace().collect();
-            if inputs[0] == "exit" {
-                break;
-            }
-            let cmd = Cmd::new(&inputs[0]).args(&inputs[1..]);
-            cmd.run();
         }
-
-
+        print!("{}: ", path.unwrap());
+        io::stdout().flush().unwrap();
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Enter a command");
+        let inputs: Vec<&str> = input.split_whitespace().collect();
+        if inputs[0] == "exit" {
+            break;
+        }
+        let cmd = Cmd::new(&inputs[0]).args(&inputs[1..]);
+        cmd.run();
+    }
 }
