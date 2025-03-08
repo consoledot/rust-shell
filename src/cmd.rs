5e7ab122951bf985ac1a05aa7a8d975c5d015704
Abimbola Oladele <31113550+consoledot@users.noreply.github.com>
2025-03-08 22:16:10 +0100

    chore: remork list

--- src/cmd.rs
+++ src/cmd.rs
@@ -1,4 +1,4 @@
-use std::{ env, fs::{ self, read_dir, File }, io, path::PathBuf };
+use std::{ env, fs::{ self, read_dir, File }, io, path::{ PathBuf, Path } };
 
 pub struct Cmd {
     command: String,
@@ -35,7 +35,7 @@ impl Cmd {
             "torch" => self.create_file(),
             "rm" => self.delete_file(),
             "mkdir" => self.create_directory(),
-            "rmdir" => self.delete_directory(),
+            // "rmdir" => self.delete_directory(),
             _ => println!("Unknown command"),
         }
     }
@@ -49,30 +49,48 @@ impl Cmd {
             }
         }
     }
+    // fn peak_directory(&self) {
+    //     let mut files: Vec<PathBuf> = Vec::new();
+    //     let path = Self::get_current_dir();
+    //     for entry in read_dir(path).unwrap() {
+    //         let entry = entry.unwrap();
+    //         let path = entry.path();
+
+    //         if path.is_file() {
+    //             files.push(path);
+    //         }
+    //     }
+    //     for file in files {
+    //         println!(
+    //             "{}",
+    //             file
+    //                 .components()
+    //                 .last()
+    //                 .map(|component| component.as_os_str().to_str())
+    //                 .flatten()
+    //                 .unwrap()
+    //         );
+    //     }
+
+    //     // Ok(())
+    // }
+
     fn peak_directory(&self) {
-        let mut files: Vec<PathBuf> = Vec::new();
         let path = Self::get_current_dir();
-        for entry in read_dir(path).unwrap() {
-            let entry = entry.unwrap();
-            let path = entry.path();
+        Self::peak_directory_recursive(&path);
+    }
 
-            if path.is_file() {
-                files.push(path);
+    fn peak_directory_recursive(path: &Path) {
+        if let Ok(entries) = read_dir(path) {
+            for entry in entries.flatten() {
+                let path = entry.path();
+                let file_name = entry.file_name().to_string_lossy().into_owned();
+
+                println!("{}", file_name);
             }
+        } else {
+            eprintln!("Failed to read directory: {:?}", path);
         }
-        for file in files {
-            println!(
-                "{}",
-                file
-                    .components()
-                    .last()
-                    .map(|component| component.as_os_str().to_str())
-                    .flatten()
-                    .unwrap()
-            );
-        }
-
-        // Ok(())
     }
 
     fn create_file(&self) {
@@ -121,19 +139,50 @@ impl Cmd {
             None => { println!("File not found") }
         }
     }
-    fn delete_directory(&self) {
-        match &self.args {
-            Some(arg) => {
-                if arg.is_empty() {
-                    println!("Enter a valid directory name");
-                    return;
-                }
-                match fs::remove_dir(arg[0].clone()) {
-                    Ok(file) => (),
-                    Err(err) => println!("enter a valid directory name"),
-                }
-            }
-            None => { println!("File not found") }
-        }
-    }
+    // fn delete_directory(&self) {
+    //     self.handle_file_operation(fs::read_dir, "directory");
+    // }
+    // fn handle_files<F>(&self, handler: F, msg: &str) -> io::Result<()>
+    //     where F: FnOnce(String) -> io::Result<String>
+    // {
+    //     match &self.args {
+    //         Some(arg) => {
+    //             if arg.is_empty() {
+    //                 println!("{}", msg);
+    //                 return Ok(());
+    //             }
+    //             match handler(arg[0].clone()) {
+    //                 Ok(_) => Ok(()),
+    //                 Err(_) => {
+    //                     println!("{}", msg);
+    //                     Err(_)
+    //                 }
+    //             }
+    //         }
+    //         None => {
+    //             println!(" not found");
+    //             Ok(())
+    //         }
+    //     }
+    // }
+
+    // fn handle_file_operation(&self, operation: fn(&str) -> io::Result<()>, entity_type: &str) {
+    //     match &self.args {
+    //         Some(args) => {
+    //             if args.is_empty() {
+    //                 println!("Enter a valid {} name", entity_type);
+    //                 return;
+    //             }
+    //             match operation(&args[0]) {
+    //                 Ok(_) => (),
+    //                 Err(_) => {
+    //                     println!("Enter a valid {} name", entity_type);
+    //                 }
+    //             }
+    //         }
+    //         None => {
+    //             println!("{} not found", entity_type);
+    //         }
+    //     }
+    // }
 }
